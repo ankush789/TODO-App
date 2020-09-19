06c9cfd25af35513c0dbd075192cc79c36582ef1
ankush789 <ankushsharma.4771@gmail.com>
2020-09-19 08:41:50 +0530

    Setting express router

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const port = 9000;
 const app = express();
 
+//Setting Express Router
+app.use('/', require('./routes'));
 
-//Setting app to listen at thr port
+//Setting app to listen at the port
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running server: ${err}`);
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+
+console.log('routes loaded');
+
+
+//Exporting Routes
+module.exports = router;
